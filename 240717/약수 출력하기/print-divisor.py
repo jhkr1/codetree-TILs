e664d4fe8c4261f61d340ece69d00fc50df4641d
jhkr1 <110290060+jhkr1@users.noreply.github.com>
2024-07-17 15:49:26 +0900

    Title: 약수 출력하기, Time: 187ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/약수 출력하기/print-divisor.py
+++ 240717/약수 출력하기/print-divisor.py
@@ -1,8 +1,16 @@
-n = int(input())
-result_list = []
-for i in range(1, int(n+1 ** (1/2))):
-    if(n % i == 0):
-        result_list.append(i)
+import math
 
-for i in result_list:
-    print(i, end=' ')
+def print_divisors(n):
+    divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:  # i와 n // i가 다를 때만 추가
+                divisors.append(n // i)
+    divisors.sort()  # 정렬
+    for divisor in divisors:
+        print(divisor, end=' ')
+
+# 정수 n 입력 받기
+n = int(input())
+print_divisors(n)
