9ed7333eb39550ca192c8f88bfd90849ba8f2a1a
jhkr1 <110290060+jhkr1@users.noreply.github.com>
2024-07-25 13:42:47 +0900

    Title: 수열 조작, Time: 248ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240725/수열 조작/sequence-manipulation.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+dq = deque()
+
+n = int(input())
+
+
+for i in range(1, n+1):
+    dq.append(i)
+
+while len(dq) > 2:
+    dq.popleft()
+    dq.append(dq[0])
+    dq.popleft()
+
+print(dq.pop())
